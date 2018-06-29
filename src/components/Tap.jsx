91b9fb72a3f8118f4191be8fdedc2e57bd976274
Ami <ik4rus.info@gmail.com>
2018-06-29 10:52:13 -0700

    start tap

--- src/components/Tap.jsx
+++ src/components/Tap.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Tap(){
+function Tap(props){
 
   return(
     <div>
+      <h4>{props.name} by {props.brewer}</h4>
+      <h5>A {props.description}</h5>
+      <h5>ABV: {props.abv} | Price: ${props.price} | Remaining: {props.remaining} pints</h5>
     </div>
   );
 }
-
+Tap.propTypes ={
+  name: PropType.string
+}
 export default Tap;
