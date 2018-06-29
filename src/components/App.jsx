8f9c00530c238d9045e8cf133e8f0617b97cb324
Ami <ik4rus.info@gmail.com>
2018-06-29 08:22:13 -0700

    instantiate template

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App(){
+  return(
+    <div>
+      <style jsx global>{`
+        *{
+          margin:0;
+          padding:0;
+        }
+      `}</style>
+      <p>hello world</p>
+    </div>
+  );
+}
+
+export default App
